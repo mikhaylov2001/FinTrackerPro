d7a86d32e2363b7827f99388fdec0ebd56828f28
dimon9995 <mikhailov1500@mail.ru>
2026-02-19 13:17:45 +0300

    updated pages

--- src/main/resources/db/migration/V8__add_first_last_name_to_users.sql
+++ src/main/resources/db/migration/V8__add_first_last_name_to_users.sql
@@ -1,5 +1,3 @@
 ALTER TABLE users
     ADD COLUMN IF NOT EXISTS first_name varchar(255),
     ADD COLUMN IF NOT EXISTS last_name varchar(255);
-    ADD COLUMN display_currency VARCHAR(3) NOT NULL DEFAULT 'RUB';
-    ADD COLUMN hide_amounts BOOLEAN NOT NULL DEFAULT FALSE;
