8544b78560bc13c63e37b1029c4b626aa235325c
dimon9995 <mikhailov1500@mail.ru>
2026-02-26 14:31:15 +0300

    добавляю классы для восстановления пароля

--- src/main/java/com/example/fintrackerpro/service/EmailService.java
+++ src/main/java/com/example/fintrackerpro/service/EmailService.java
@@ -1,21 +1,13 @@
 package com.example.fintrackerpro.service;
 
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Profile;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
     private static final Logger log = LoggerFactory.getLogger(EmailService.class);
-    private final JavaMailSender mailSender;
 
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
 
     public void sendPasswordResetEmail(String to, String username, String resetLink) {
         String body = buildResetEmailText(username, resetLink);
